707095c8f0daf2dd7d85d67f893ff4e90c7c9297
gbe-zerodata <errante.giovanbattista@zerodata.dev>
2025-06-05 16:47:51 +0900

    updated logging and CI

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"io"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 func handleConnection(clientConn net.Conn, targetAddress string) {
@@ -54,40 +57,47 @@ func handleConnection(clientConn net.Conn, targetAddress string) {
 }
 
 func main() {
+	logfilename := "gotunnel_" + time.Now().Format("20060102_150405") + ".log"
+	logFile, err := os.OpenFile(filepath.Join(os.TempDir(), logfilename), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Failed to open log file: %v", err)
+	}
+	defer logFile.Close()
+
+	log.Printf("Logging to %s", logFile.Name())
+	log.SetFlags(log.Ldate | log.Ltime)
+	log.SetOutput(io.MultiWriter(logFile, os.Stdout))
 
 	var listenAddress string
 	var connectAddress string
 
 	flag.StringVar(&listenAddress, "listen", "", "Address to listen on (format: host:port or :port for all interfaces)")
-	flag.StringVar(&connectAddress, "connect", "", "Target address to forward requests to (format: host:port)")
+	flag.StringVar(&connectAddress, "connect", "", "Target address to forward requests to (format: host:port or :port for all interfaces)")
 	flag.Parse()
 
-	if listenAddress == "" {
-		log.Fatal("Listen address must be specified in the format host:port or :port for all interfaces")
-	}
-
-	if connectAddress == "" {
-		log.Fatal("Target address must be specified in the format host:port")
+	if listenAddress == "" || connectAddress == "" {
+		flag.Usage()
+		return
 	}
 
 	listener, err := net.Listen("tcp", listenAddress)
 	if err != nil {
-		log.Fatalf("Failed to listen on %s: %v", listenAddress, err)
+		log.Fatalf("Failed to listen on %s: \n%v\n", listenAddress, err)
 	}
 	defer listener.Close()
 
-	log.Printf("Listening on %s, forwarding to %s", listenAddress, connectAddress)
+	log.Printf("Listening on %s, forwarding to %s\n", listenAddress, connectAddress)
 
 	for {
 		clientConn, err := listener.Accept()
 		if err != nil {
-			log.Printf("Failed to accept connection: %v", err)
+			log.Printf("Failed to accept connection: \n%v\n", err)
 			continue
 		}
 
 		go handleConnection(clientConn, connectAddress)
 
-		log.Printf("Accepted connection from %s", clientConn.RemoteAddr())
+		log.Printf("Accepted connection from %s\n", clientConn.RemoteAddr())
 	}
 
 }
